c9ca98adcabf1e6ad547b62c31e18a8721633f47
TheFranzy <galen.housego@gmail.com>
2019-11-04 14:30:15 -0600

    inserted image module

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,25 +1,15 @@
 defmodule Identicon do
-  @moduledoc """
-  Documentation for Identicon.
-  """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Identicon.hello()
-      :world
-
-  """
   def main(input) do
     input
     |> hash_input
   end
 
   def hash_input(input) do
-    :crypto.hash(:md5, input)
+    hex = :crypto.hash(:md5, input)
     |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
   end
 
 end
